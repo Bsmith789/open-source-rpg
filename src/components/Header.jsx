5e5facd5088a5b5caa271de395ddb39143cf56dc
Gabriel Lemos de Toledo <gabriel.lemos.toledo@everis.nttdata.com>
2022-01-10 01:27:29 -0300

    Adicionando imagem

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,7 +17,7 @@ const Header = ({
             style={{ textAlign: 'center', marginTop: 50 }}
         >
             <Image
-                src="/assets/Neterise.svg"
+                src="/assets/logoEquilibrio.svg"
                 alt="Destruição do Equilíbrio"
                 width="150"
                 height="55"
